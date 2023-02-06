58476a0734503fa9499662da6782673cc193b17d
Bartosz Kucharski <bartosz.kucharski@providers.glovoapp.com>
2023-02-06 11:29:18 +0100

    Fix bug with RepositoriesURL

--- GitHubSearch/Services/RepositoriesClient/RepositoriesClient+Live.swift
+++ GitHubSearch/Services/RepositoriesClient/RepositoriesClient+Live.swift
@@ -14,11 +14,9 @@ extension RepositoriesClient {
         /// SearchForRepositoriesId acts as unique id (hash value). It could be plain string also.
         /// However it adds extra protection, as it's almost impossible to duplicate it.
         struct SearchForRepositoriesId: Hashable {}
-        var urlRequest = URLRequest(url: URL(string: "https://api.github.com/search/repositories?q=\(phrase)")!)
-        urlRequest.addValue("application/vnd.github.v3+json", forHTTPHeaderField: "accept")
         
         return URLSession.shared
-            .dataTaskPublisher(for: urlRequest)
+            .dataTaskPublisher(for: getRepositoriesURLRequest(phrase: phrase))
             .eraseToEffect()
             .cancellable(id: SearchForRepositoriesId(), cancelInFlight: true)
             .map(\.data)
@@ -27,4 +25,20 @@ extension RepositoriesClient {
             .catchToEffect()
     }
     
+}
+
+private extension RepositoriesClient {
+
+  static func getRepositoriesURLRequest(phrase: String) -> URLRequest {
+    var urlComponents = URLComponents()
+    urlComponents.scheme = "https"
+    urlComponents.host = "api.github.com"
+    urlComponents.path = "/search/repositories"
+    urlComponents.queryItems = [.init(name: "q", value: phrase)]
+
+    var urlRequest = URLRequest(url: urlComponents.url!)
+    urlRequest.addValue("application/vnd.github.v3+json", forHTTPHeaderField: "accept")
+
+    return urlRequest
+  }
 }
